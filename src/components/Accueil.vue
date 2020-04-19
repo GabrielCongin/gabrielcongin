ab63ca8c9e4c209064d9a4ecdb3939944bc6890b
Gabriel <gabriel.congin@gmail.com>
2020-04-19 14:55:02 +0200

    test résolution

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -65,7 +65,7 @@
             padding-left: 36%;
         }
     }
-    @media all and (min-width: 1680px){
+    @media all and (min-width: 1440px){
         #perso{
             width:300px;
         }
